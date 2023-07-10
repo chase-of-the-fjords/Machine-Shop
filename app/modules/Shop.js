9a125c8e99da3e6cac1efd76769d9ae0376d04ad
chase-of-the-fjords <cep.piano@gmail.com>
2023-07-10 15:36:09 -0700

    Moved Shop to its own file

---
+++ app/modules/Shop.js
@@ -0,0 +1,60 @@
+'use client'
+
+import Building from './Building';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+import styles from './App.module.css';
+
+export default function Shop() {
+    const [shops, setShops] = useState([]);
+    const [machines, setMachines] = useState([]);
+
+    async function getShops() {
+        const postData = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/shops`, postData);
+        const response = await res.json();
+        setShops(response);
+    }
+
+    async function getMachines() {
+        const postData = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/machines`, postData);
+        const response = await res.json();
+        setMachines(response);
+    }
+
+    useEffect(() => {
+        getShops();
+        getMachines();
+    }, []);
+
+    return (
+        <div className={styles.shop}>
+            {
+                shops
+                .filter((shop) => {
+                    return (shop.enabled == 1);
+                })
+                .map((shop) => {
+                    return <Building key={shop.code} data={shop} machines={
+                        machines.filter((machine) => {
+                            return (machine.shop == shop.code);
+                        })
+                    }/>
+                })
+            }
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,52 +1,13 @@
-'use client'
-
-import Machine from './modules/Machine';
-import Building from './modules/Building';
+import Shop from './modules/Shop';
 
 import styles from './modules/App.module.css';
 
-import { useEffect } from 'react';
-import { useState } from 'react';
-
 // The full main page
 export default function App() {
-    const [shops, setShops] = useState([]);
-    const [machines, setMachines] = useState([]);
-
-    async function getShops() {
-        const postData = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            request: "machines",
-        }
-        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/shops`, postData);
-        const response = await res.json();
-        setShops(response);
-    }
-
-    async function getMachines() {
-        const postData = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }
-        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/machines`, postData);
-        const response = await res.json();
-        setMachines(response);
-    }
-
-    useEffect(() => {
-        getShops();
-        getMachines();
-    }, []);
-
     return (
         <>
             <Menu></Menu>
-            <Shop shops={shops} machines={machines}></Shop>
+            <Shop></Shop>
         </>
     );
 }
@@ -57,24 +18,4 @@ function Menu() {
     style={{
         
     }}>Origin Golf Machine Shop</h1>
-}
-
-function Shop({shops, machines}) {
-    return (
-        <div className={styles.shop}>
-            {
-                shops
-                .filter((shop) => {
-                    return (shop.enabled == 1);
-                })
-                .map((shop) => {
-                    return <Building key={shop.code} data={shop} machines={
-                        machines.filter((machine) => {
-                            return (machine.shop == shop.code);
-                        })
-                    }/>
-                })
-            }
-        </div>
-    )
 }
