da0aa4c8ef79d0882c39bc27230f66bd8e40773f
chase-of-the-fjords <cep.piano@gmail.com>
2023-07-14 15:56:10 -0700

    Modified popup box

--- app/modules/InformationBox.js
+++ app/modules/InformationBox.js
@@ -22,46 +22,47 @@ export default function InformationBox( { doAction, popupState, machine, jobs } 
         <div className={styles.popup_div}>
             <div className={styles.background} onClick={() => { doAction("closePopup", []) }} />
             <div className={styles.info_box}>
-                <h1 className={styles.machine_name}>{machine.name}</h1>
-                { machine.state == 1 && <h2 className={styles.state}>OUT OF SERVICE</h2> }
-                { machine.state == 2 && <h2 className={styles.state}>PRIORITY</h2> }
+                <div className={styles.content}>
+                    <h1 className={styles.machine_name}>{machine.name}</h1>
+                    { machine.state == 1 && <h2 className={styles.state}>OUT OF SERVICE</h2> }
+                    { machine.state == 2 && <h2 className={styles.state}>PRIORITY</h2> }
 
-                { /* The list of current jobs. */ }
-                { currentJobs.length > 0 &&
-                <>
-                    <h3 className={styles.subsection}>Currently Running:</h3>
-                    <ul className={styles.list}>
-                    {currentJobs.map((job) => {
-                        return <><li>{job.op}</li>
-                        {job.notes == null || <p className={styles.note}>{job.notes}</p>}</>
-                    })}
-                    </ul>
-                </>}
-                
-                { /* The list of jobs on hold. */ }
-                { onHoldJobs.length > 0 &&
-                <>
-                    <h3 className={styles.subsection}>On Hold:</h3>
-                    <ul className={styles.list}>
-                    {onHoldJobs.map((job) => {
-                        return <><li>{job.op}</li>
-                        {job.notes == null || <p className={styles.note}>{job.notes}</p>}</>
-                    })}
-                    </ul>
-                </>}
+                    { /* The list of current jobs. */ }
+                    { currentJobs.length > 0 &&
+                    <>
+                        <h3 className={styles.subsection}>Currently Running:</h3>
+                        <ul className={styles.list}>
+                        {currentJobs.map((job) => {
+                            return <><li>{job.op}</li>
+                            {job.notes == null || <p className={styles.note}>{job.notes}</p>}</>
+                        })}
+                        </ul>
+                    </>}
+                    
+                    { /* The list of jobs on hold. */ }
+                    { onHoldJobs.length > 0 &&
+                    <>
+                        <h3 className={styles.subsection}>On Hold:</h3>
+                        <ul className={styles.list}>
+                        {onHoldJobs.map((job) => {
+                            return <><li>{job.op}</li>
+                            {job.notes == null || <p className={styles.note}>{job.notes}</p>}</>
+                        })}
+                        </ul>
+                    </>}
 
-                { /* The list of jobs queued. */ }
-                { queuedJobs.length > 0 &&
-                <>
-                    <h3 className={styles.subsection}>Queued:</h3>
-                    <ul className={styles.list}>
-                    {queuedJobs.map((job) => {
-                        return <><li>{job.op}</li>
-                        {job.notes == null || <p className={styles.note}>{job.notes}</p>}</>
-                    })}
-                    </ul>
-                </>}
-
+                    { /* The list of jobs queued. */ }
+                    { queuedJobs.length > 0 &&
+                    <>
+                        <h3 className={styles.subsection}>Queued:</h3>
+                        <ul className={styles.list}>
+                        {queuedJobs.map((job) => {
+                            return <><li>{job.op}</li>
+                            {job.notes == null || <p className={styles.note}>{job.notes}</p>}</>
+                        })}
+                        </ul>
+                    </>}
+                </div>
             </div>
         </div>
     )
