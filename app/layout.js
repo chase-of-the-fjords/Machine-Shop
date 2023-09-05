8d24afae66e4379c5c6b33846866b5c4878aa104
chase-of-the-fjords <cep.piano@gmail.com>
2023-09-04 21:57:05 -0700

    Added font support + CastleTLig

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import localFont from 'next/font/local';
 
 const inter = Inter({ subsets: ['latin'] })
+
+const CastleTLig = localFont({
+  src: '../public/fonts/CastleTLig.ttf',
+  variable: '--font-castletlig'
+})
 
 export const metadata = {
   title: 'Origin Golf Machine Shop',
@@ -11,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${CastleTLig.variable}`}>{children}</body>
     </html>
   )
 }
