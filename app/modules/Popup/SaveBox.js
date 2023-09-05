3b4e9f2e2b9e0283924f6110023fefdc74a6ec20
chase-of-the-fjords <cep.piano@gmail.com>
2023-09-05 12:40:59 -0700

    SaveBox converted to Tailwind

--- app/modules/Popup/SaveBox.js
+++ app/modules/Popup/SaveBox.js
@@ -10,6 +10,6 @@ export default function SaveBox() {
 
         // JSX (RETURN VALUE)
 
-    return <div className={styles.background}/>
+    return <div className="fixed w-full h-[120%] bg-black opacity-50" />
 
 }
