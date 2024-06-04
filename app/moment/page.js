c1c4756cd4ad3884d2d4173e4484daf803904e95
Chase Peterson <cep.piano@gmail.com>
2024-06-04 16:14:35 -0700

    Fixed moment page

--- app/moment/page.js
+++ app/moment/page.js
@@ -1,56 +1,91 @@
 // This is an interactive component, so it's a client component.
-'use client'
+"use client";
 
 // Imports the shop component.
-import Shop from './Shop';
+import Shop from "./Shop";
 
 // Basic React hooks.
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 /**
  * The default export for the edit page.
- * 
+ *
  * @returns JSX representation of the edit page.
  */
 export default function App() {
+	// These 3 hooks contain the buildings, machines, and jobs.
+	const [buildings, setBuildings] = useState([]);
+	const [machines, setMachines] = useState([]);
+	const [jobs, setJobs] = useState([]);
 
-    // These 3 hooks contain the buildings, machines, and jobs.
-    const [buildings, setBuildings] = useState([]);
-    const [machines, setMachines] = useState([]);
-    const [jobs, setJobs] = useState([]);
+	const [datetime, setDatetime] = useState("");
 
-    const [datetime, setDatetime] = useState("");
+	// Tracks the user editing based on the password form.
+	const [user, setUser] = useState({
+		name: "",
+		id: 0,
+		password: "",
+		active: 0,
+	});
 
+	// Tracks if the user has any unsaved changes.
+	const [hasChanges, setHasChanges] = useState(false);
 
+	// Prevents reloading/leaving if there are unsaved changes.
+	useEffect(() => {
+		const unloadCallback = (event) => {
+			if (hasChanges) {
+				event.preventDefault();
+				event.returnValue = "";
+				return "";
+			}
+		};
 
+		window.addEventListener("beforeunload", unloadCallback);
+		return () => window.removeEventListener("beforeunload", unloadCallback);
+	}, [hasChanges]);
 
-        // JSX (RETURN VALUE)
+	// JSX (RETURN VALUE)
 
-    return (<>
+	return (
+		<>
+			{/* BACKGROUND */}
+			<div className="bg-cool-grey-100 -z-10 fixed top-0 left-0 w-full h-[150%]"></div>
 
-            {/* BACKGROUND */}
-            <div className="bg-blue-300 -z-10 fixed top-0 left-0 w-full h-[150%]"></div>
-
-            {/* FOREGROUND */}
-            <div>
-                { /* The menu at the top of the screen. Likely to be updated later. */ }
-                <Menu setDatetime={setDatetime} />
-
-                { /* The rest of the machine shop. */ }
-                <Shop type="moment" buildings={buildings} machines={machines} jobs={jobs} 
-                    setBuildings={setBuildings} setMachines={setMachines} setJobs={setJobs}
-                    datetime={datetime} />
-            </div>
-
-        </>
-    );
+			{/* FOREGROUND */}
+			<div>
+				{/* The rest of the machine shop. */}
+				<Shop
+					type="moment"
+					buildings={buildings}
+					machines={machines}
+					jobs={jobs}
+					setBuildings={setBuildings}
+					setMachines={setMachines}
+					setJobs={setJobs}
+					user={user}
+					setUser={setUser}
+					hasChanges={hasChanges}
+					setHasChanges={setHasChanges}
+					datetime={datetime}
+					setDatetime={setDatetime}
+				/>
+			</div>
+		</>
+	);
 }
 
 // The menu bar component.
-function Menu( {setDatetime} ) {
-
-    return <div>
-        <input className="block mx-auto my-6 text-xl" type="datetime-local" onChange={(e) => { setDatetime(e.target.value) } } />
-    </div>
-
+function Menu({ setDatetime }) {
+	return (
+		<div>
+			<input
+				className="block mx-auto my-6 text-xl"
+				type="datetime-local"
+				onChange={(e) => {
+					setDatetime(e.target.value);
+				}}
+			/>
+		</div>
+	);
 }
