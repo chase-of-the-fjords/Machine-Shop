078bd8fd0bc0f0118df588f8ff57b95406163ab3
Chase Peterson <cep.piano@gmail.com>
2024-06-22 11:26:42 -0700

    Created initial version of new history page

---
+++ app/newhistory/page.js
@@ -0,0 +1,155 @@
+// This is an interactive component, so it's a client component.
+"use client";
+
+// Basic React hooks.
+import { useState, useEffect } from "react";
+
+import Link from "next/link";
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import ClusterHeading from "./components/ClusterHeading";
+
+import { getClusterLog } from "./Helpers/Interface";
+import Action from "./components/Action";
+
+/**
+ * The default export for the history page.
+ *
+ * @returns JSX representation of the history page.
+ */
+export default function App() {
+	// JSX (RETURN VALUE)
+
+	return (
+		<>
+			{/* BACKGROUND */}
+			<div className="bg-cool-grey-100 -z-10 fixed top-0 left-0 w-full h-[150%]"></div>
+
+			<Menu />
+
+			<History />
+		</>
+	);
+}
+
+// The menu bar component.
+function Menu() {
+	const [show, setShow] = useState(true);
+	const [lastScrollY, setLastScrollY] = useState(0);
+
+	const controlNavbar = () => {
+		if (window.scrollY > lastScrollY) {
+			// if scroll down hide the navbar
+			setShow(false);
+		} else {
+			// if scroll up show the navbar
+			setShow(true);
+		}
+
+		// remember current page location to use in the next move
+		setLastScrollY(window.scrollY);
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", controlNavbar);
+
+		// cleanup function
+		return () => {
+			window.removeEventListener("scroll", controlNavbar);
+		};
+	}, [lastScrollY]);
+
+	const [loginPopup, setLoginPopup] = useState(false);
+
+	return (
+		<>
+			<div className="invisible h-16 font-RobotoMono" />
+			<div
+				className={`fixed z-50 w-screen h-16 m-auto shadow-xl bg-cool-grey-50 transition-[top] ${
+					show ? "top-0" : "-top-20"
+				}`}
+			>
+				<div className="relative max-w-[1000px] mx-auto">
+					<div className="absolute invisible w-full mx-auto mt-1 text-lg font-semibold text-center sm:visible top-4">
+						Shop History
+					</div>
+					<Link
+						href="./"
+						className="absolute mt-2 ml-4 cursor-pointer w-fit h-fit"
+					>
+						<img src="./inverted-logo.png" className="h-12" />
+					</Link>
+
+					<div>
+						<DropdownMenu>
+							<DropdownMenuTrigger className="absolute mt-1 mr-8 font-semibold transition-colors cursor-pointer right-1 top-4 hover:text-cool-grey-900 text-cool-grey-500">
+								Menu
+							</DropdownMenuTrigger>
+							<DropdownMenuContent>
+								<Link href="./">
+									<DropdownMenuItem>View Shop</DropdownMenuItem>
+								</Link>
+								<Link href="./moment">
+									<DropdownMenuItem>Moment History</DropdownMenuItem>
+								</Link>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+function History() {
+	const [log, setLog] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			let start = new Date("03/01/2024").toLocaleDateString("sv");
+			let end = new Date(Date.now()).toLocaleDateString("sv");
+			setLog(await getClusterLog(start, end));
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<div className="flex max-w-[1000px] mx-auto my-6 space-x-6 px-6">
+				<div className="w-64 h-64 rounded-md shadow-lg bg-cool-grey-50"></div>
+				<div className="flex-grow p-6 rounded-md shadow-lg h-fit bg-cool-grey-50">
+					{log.map((cluster) => {
+						return (
+							<div key={cluster.date + " " + cluster.time + " " + cluster.user}>
+								<ClusterHeading
+									name={cluster.user}
+									date={cluster.date}
+									time={cluster.time}
+								/>
+								<div className="m-4">
+									{Object.entries(cluster.entries).map(([key, value]) => (
+										<div className="mb-4 font-Poppins" key={key}>
+											<h3 className="text-lg font-semibold text-cool-grey-900">
+												{key}
+											</h3>
+											{value.map((action, i) => (
+												<Action action={action} key={i} />
+											))}
+										</div>
+									))}
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</>
+	);
+}
